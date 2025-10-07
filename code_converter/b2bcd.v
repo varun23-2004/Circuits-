9185206d0ce5214a1b0871057d73d1599946cd0a
varun23-2004 <146314309+varun23-2004@users.noreply.github.com>
2025-10-07 21:40:22 +0530

    Delete code_converter directory

--- code_converter/b2bcd.v
+++
@@ -1,9 +0,0 @@
-module b2bcd(bin, bcd);
-input  [3:0] bin;     
-output [3:0] bcd;    
-
-assign bcd[3] = (bin[3] & (bin[2] | bin[1])) ;
-assign bcd[2] = (bin[3] & ~bin[2] & bin[1]) | (bin[2] & bin[1]) | (bin[2] & ~bin[3]);
-assign bcd[1] = (~bin[3] & bin[1]) | (bin[3] & ~bin[2]);
-assign bcd[0] = bin[0] ;
-endmodule
--- code_converter/bcd2exce.v
+++
@@ -1,6 +0,0 @@
-module bcd2exce (bcd,exce);
-input [15:0] bcd;
-output [15:0] exce;
-
-assign exce=bcd + 16'h3;
-endmodule
